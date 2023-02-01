89e96900792ba9bf6e86ae6b188d09397a63c322
Lukasz <lukasz.malinowski79@gmail.com>
2023-02-01 12:34:07 +0100

    Przypisanie wartości oceny klubów do meczów w które te kluby biorą udział, dzięki temu możemy porównywać oceny do pary klubów, które będą odbywały ze sobą mecz, dodatkowo kosmetyczne zmiany w listach, usunięcie białych znaków, które tworzyły niepożądane efekty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import random
 
 excel_file= 'EkstraklasaMoc.xlsx'
 
@@ -15,12 +16,46 @@ overall = dict(zip(kluby, suma))
 
 print(overall)
 
-overall = {
-    klub.replace(' ',''): ov
-    for klub, ov in overall.items()
-}
+# overall = {
+#     klub.replace(' ',''): ov
+#     for klub, ov in overall.items()
+# }
 
-for klub, ov in overall.items():
-    exec(f"{klub}={repr(ov)}")
+# for klub, ov in overall.items():
+#     exec(f"{klub}='{ov}'")
 
-print(ZagłębieLubin)
+
+excel_file_2= 'EkstraklasaMecze.xlsx'
+
+em = pd.read_excel(excel_file_2, usecols=["TEAM1","TEAM2"])
+
+
+
+
+gospodarz = em['TEAM1'].values.tolist()
+gość = em['TEAM2'].values.tolist()
+
+
+# mecze = [m for m in zip(gospodarz, gość)]
+
+mecze = [m for pair in zip(gospodarz,gość) for m in pair]
+
+
+for i, klub in enumerate(mecze):
+    mecze[i] = klub.strip()
+
+print(mecze)
+
+
+
+mecze_moc = [0] * len(mecze)
+
+for i, item in enumerate(mecze):
+    for klub in overall:
+        if item[:5] == klub[:5]:
+            mecze_moc[i] = overall[klub]
+            break
+
+
+print(mecze_moc)
+
