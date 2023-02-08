ae3f0c3c251b509886109b955b18a8f35eec46a5
Lukasz <lukasz.malinowski79@gmail.com>
2023-02-08 22:22:31 +0100

    Uporządkowanie całego pliku jako menu główne i sporządkowanie działania programu jako funkcje w menu.

--- functions.py
+++ functions.py
@@ -1,17 +1,172 @@
 
+def linia():
+    print("====================================")
+
+
+def path1():
+    import os
+    return os.path.dirname(os.path.abspath("MeczeRezultaty.xlsx"))
+
+def path2():
+    import os
+    return os.path.dirname(os.path.abspath("Wynik końcowy na koniec sezonu.xlsx"))
+
+
+def closeExcel():
+    import win32com.client as win
+    x = path1()
+    y = path2()
+    path_excel_1 = f"{x}\MeczeRezultaty.xlsx"
+    path_excel_2 = f"{y}\Wynik końcowy na koniec sezonu"
+
+
+    x1 = win.gencache.EnsureDispatch("Excel.Application")
+    x2 = x1.Workbooks.Open(path_excel_1)
+    x2.Close(SaveChanges=False)
+    x1.Quit()
+    menu()
+
 
 def start():
-    import os
-    print("Dzień dobry! Przedstawiam program współpracujący z arkuszami kalkulacyjnymi, mający na celu sprawdzić przewidywany tegoroczny sezon Ekstraklasy. Jeśli chcemy poznać now")
-
-
-
+    print("Dzień dobry! Przedstawiam program współpracujący z arkuszami kalkulacyjnymi, mający na celu sprawdzić przewidywany tegoroczny sezon Ekstraklasy. Wybierz jedną z opcji aby przejść dalej.")
 
 def menu():
+    linia()
     print("Menu Główne")
+    linia()
+    print("1. Rozpocznij przewidywania sezonu")
+    print("2. Poznaj siłę drużyn")
+    print("3. Wyłącz arkusz z rezultatami")
+    print("4. Wyjście")
+    wybor = int(input("Wybierz opcję [1-4]: "))
+    if wybor == 1:
+        opcja_1()
+    elif wybor == 2:
+        opcja_2()
+    elif wybor == 3:
+        closeExcel()
+    elif wybor == 4:
+        wyjscie()
+    else:
+        print("Nieprawidłowa wartość. Spróbuj ponownie.")
+        menu()
+
+
+def opcja_1():
+
+    import pandas as pd
+    import numpy as np
+    import os
 
 
 
+
+    excel_file= 'EkstraklasaMoc.xlsx'
+    excel_file_delete = "MeczeRezultaty.xlsx"
+
+
+    try:
+        if os.path.exists(excel_file_delete):
+            os.remove(excel_file_delete)
+            print(f"Plik {excel_file_delete} został usunięty pomyślnie.")
+        else:
+            print(f"Plik {excel_file_delete} nie istnieje.")
+    except PermissionError:
+        print("Plik MeczeRezultaty.xlsx jest otwarty, wybierz najpierw opcję 3, aby go usunąć.")
+        menu()
+
+    es = pd.read_excel(excel_file, usecols=["KLUB","SUMA"])
+
+    kluby = es['KLUB'].values.tolist()
+    suma = es['SUMA'].values.tolist()
+
+
+    overall = dict(zip(kluby, suma))
+
+
+
+    excel_file_2= 'EkstraklasaMecze.xlsx'
+
+    em = pd.read_excel(excel_file_2, usecols=["TEAM1","TEAM2"])
+
+
+
+
+    gospodarz = em['TEAM1'].values.tolist()
+    gość = em['TEAM2'].values.tolist()
+
+
+    # mecze = [m for m in zip(gospodarz, gość)]
+
+    mecze = [m for pair in zip(gospodarz,gość) for m in pair]
+
+
+    for i, klub in enumerate(mecze):
+        mecze[i] = klub.strip()
+
+
+
+    mecze_moc = [0] * len(mecze)
+
+    for i, item in enumerate(mecze):
+        for klub in overall:
+            if item[:5] == klub[:5]:
+                mecze_moc[i] = overall[klub]
+                break
+
+
+    winnersi = porównywarka(mecze_moc)
+
+
+
+
+
+
+    winnersi = mocNaNazwe(winnersi, overall)
+
+
+    new_em = pd.DataFrame({"Rezultaty": winnersi})
+
+    new_em.to_excel("rezultaty.xlsx",index=False)
+
+    mix_em = pd.concat([em,new_em],axis=1)
+
+    mix_em.to_excel("MeczeRezultaty.xlsx",index=False)
+
+    excel_file_3 = "MeczeRezultaty.xlsx"
+
+    excel_file_4 = "Wynik końcowy na koniec sezonu.xlsx"
+
+
+
+
+        
+    filesOpen(excel_file_3,excel_file_4)
+
+
+    finish = pd.read_excel(excel_file_4, usecols=["KLUB","PKT"])
+
+
+    finish.sort_values(by='PKT',ascending=False, inplace=True)
+
+    print(finish.to_string(index=False))
+
+    menu()
+
+
+def opcja_2():
+    import pandas as pd
+    excel_file = "EkstraklasaMoc.xlsx"
+    moc = pd.read_excel(excel_file, usecols=["KLUB","SUMA"])
+    moc.sort_values(by='SUMA',ascending=False, inplace=True)
+    print(moc.to_string(index=False))
+    menu()
+
+
+
+
+def wyjscie():
+    pass
 
 
 
@@ -182,13 +337,10 @@ def mocNaNazwe(array, dict):
     return array
 
 
-def filesOpenAndClose(file1,file2):
+def filesOpen(file1):
     import os
     import time
     os.startfile(file1)   
-    time.sleep(1)   
-    os.startfile(file2)
-    file1.exit()
-
+    time.sleep(2)   
 
  
